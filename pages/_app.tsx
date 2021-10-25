b9b31cdf1a7557e02d740e18ed4ab9d6d2f676b8
Matheus <matheusalmeida016@gmail.com>
2021-10-25 03:24:38 -0400

    Add comics slider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,11 @@ import 'normalize.css'
 const GlobalStyle = createGlobalStyle`
   html {
     font-size: 100%;
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
   }
 
   body {
@@ -15,19 +20,29 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+const AppWrapper = styled.div`
+  display: grid;
+  min-height: 100vh;
+  grid-template-rows: min-content 1fr;
+`
+
 const Main = styled.main`
   min-height: 100%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  padding: 3em 0;
 `
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AppWrapper>
       <GlobalStyle />
       <Header />
       <Main>
         <Component {...pageProps} />
       </Main>
-    </>
+    </AppWrapper>
   )
 }
 
--- src/components/comics/comics.tsx
+++ src/components/comics/comics.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
 type ComicsType = {
   title: string
   id: number,
@@ -10,8 +13,75 @@ type ComicsProps = {
   comics: ComicsType[]
 }
 
+const Slider = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  margin: auto 0;
+`
+
+const Image = styled.img`
+  width: 100%;
+  max-width: 50vw;
+  border: 4px solid #070201;
+  margin: 0 2em;
+`
+
 export function Comics ({ comics }: ComicsProps) {
+  const length = comics.length
+  const [current, setCurrent] = useState(0)
+  const [previous, setPrevious] = useState(length - 1)
+  const [next, setNext] = useState(current + 1)
+
+  const handlePrevBtn = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+    setPrevious(previous === 0 ? length - 1 : previous - 1)
+    setNext(next === 0 ? length - 1 : next - 1)
+  }
+
+  const handleNextBtn = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+    setPrevious(previous === length - 1 ? 0 : previous + 1)
+    setNext(next === length - 1 ? 0 : next + 1)
+  }
+
+  const previousComic = comics.map((item, index) => {
+    if (index === previous) {
+      return <Image key={item.id} src={item.thumbnail} alt={item.title} />
+    }
+
+    return null
+  })
+
+  const currentComic = comics.map((item, index) => {
+    if (index === current) {
+      return <Image key={item.id} src={item.thumbnail} alt={item.title} />
+    }
+
+    return null
+  })
+
+  const nextComic = comics.map((item, index) => {
+    if (index === next) {
+      return <Image key={item.id} src={item.thumbnail} alt={item.title} />
+    }
+
+    return null
+  })
+
   return (
-    <div></div>
+    <>
+      <Slider>
+        {previousComic}
+
+        {currentComic}
+
+        {nextComic}
+      </Slider>
+
+      <button onClick={handlePrevBtn}>prev</button>
+      <button onClick={handleNextBtn}>next</button>
+    </>
   )
 }

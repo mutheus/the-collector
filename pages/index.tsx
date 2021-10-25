ddbdd21bfb6f53dc68539b37f10a4ee37dbfdc48
Matheus <matheusalmeida016@gmail.com>
2021-10-24 20:49:46 -0400

    Fetch actual data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
-import { GetStaticProps } from 'next'
+import { InferGetStaticPropsType } from 'next'
 import Head from 'next/head'
 import { api } from 'src/services/api'
 
-type HomeProps = {
-  comics: any
+type ResultsType = {
+  title: string
+  id: number,
+  description: string
+  thumbnail: {
+    extension: string
+    path: string
+  }
+  prices: { price: number }[]
 }
 
-const Home = ({ comics }: HomeProps) => {
+type DataType = {
+  data: {
+    data: {
+      results: ResultsType[]
+    }
+  }
+}
+
+const Home = ({ comics }: InferGetStaticPropsType<typeof getStaticProps>) => {
   console.log(comics)
 
   return (
@@ -23,13 +38,14 @@ const Home = ({ comics }: HomeProps) => {
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await api.get('comics', {
+export const getStaticProps = async () => {
+  const { data }: DataType = await api.get('comics', {
     params: {
       ts: process.env.NEXT_PUBLIC_TIMESTAMP,
       apikey: process.env.NEXT_PUBLIC_API_KEY,
       hash: process.env.NEXT_PUBLIC_HASH,
-      limit: 5,
+      formatType: 'comic',
+      orderBy: 'issueNumber',
     },
     proxy: {
       host: '192.168.43.1',
@@ -37,9 +53,23 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   })
 
+  const rawComicsData = data.data.results.map(elem => {
+    return {
+      id: elem.id,
+      title: elem.title,
+      description: elem.description,
+      thumbnail: `${elem.thumbnail.path}.${elem.thumbnail.extension}`,
+      price: elem.prices.length === 1 && elem.prices[0].price === 0
+        ? Number((Math.random() * 9).toFixed(1) + '9')
+        : elem.prices.find(item => item.price !== 0)?.price,
+    }
+  })
+
+  const comics = rawComicsData.filter(item => !item.thumbnail.includes('not_available') && item.description !== null)
+
   return {
     props: {
-      comics: data,
+      comics,
     },
   }
 }
